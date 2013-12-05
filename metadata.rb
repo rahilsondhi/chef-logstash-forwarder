00e51e52836cd4b6ba3b09c370696119d8dd19dd
phildougherty <phil@brandingbrand.com>
2013-12-05 13:28:10 -0500

    Initial commit to change from lumberjack to logstash-forwarder

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+maintainer        "Phil Dougherty"
+maintainer_email  "phil@brandingbrand.com"
+license           "Apache 2.0"
+description       "Installs and configures Logstash-Forwarder."
+version           "0.0.1"
+recipe            "logstash-forwarder", "Installs and configures Lumberjack"
+name              "logstash-forwarder"
+
+%w{ logrotate }.each do |d|
+  depends d
+end
+
+%w{ logstash }.each do |s|
+  suggests s
+end
+
+%w{ ubuntu debian rhel centos scientific amazon fedora}.each do |os|
+    supports os
+end
